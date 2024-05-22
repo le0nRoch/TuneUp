f465b3ea8ae4c22838a84121e116f3b8dbe27094
David <bischof.david.db@gmail.com>
2024-05-22 21:26:34 +0200

    added the mobile nav

---
+++ src/app/components/NavbarMobile.tsx
@@ -0,0 +1,46 @@
+'use server'
+import { Box, Link } from '@chakra-ui/react';
+import React from 'react';
+import { FaHome, FaBell, FaPlus, FaHeart, FaUser } from 'react-icons/fa';
+import { IoIosChatbubbles } from "react-icons/io";
+
+export async function NavbarMobile() {
+    const icons = [
+        {
+            icon: <FaBell />,
+            link: '/notifications',
+        },
+        {
+            icon: <FaPlus />,
+            link: '/create',
+        },
+        {
+            icon: <IoIosChatbubbles />,
+            link: '/chats',
+        },
+        {
+            icon: <FaUser />,
+            link: '/profile',
+        },
+    ];
+
+    return (
+        <Box
+            bg="white"
+            w="100%"
+            h="60px"
+            position="fixed"
+            bottom="0"
+            display="flex"
+            justifyContent="space-around"
+            alignItems="center"
+            boxShadow="0px -1px 10px rgba(0, 0, 0, 0.1)"
+        >
+            {icons.map((item, index) => (
+                <Link key={index} href={item.link}>
+                    {item.icon}
+                </Link>
+            ))}
+        </Box>
+    );
+}
--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,7 +1,13 @@
+import { NavbarMobile } from "../components/NavbarMobile";
+
+
+
 export default function Feed() {
   return (
     <main>
       <p>feed</p>
+      wasd
+      <NavbarMobile />
     </main>
   );
 }
